7e6ccfd0bac7a6e2cca7ff9eeb38ad9d528851e4
daddeo <none@none.com>
2013-03-27 20:40:47 -0500

    BranchedTest1
    Changes in a branch

--- Test.java
+++ Test.java
@@ -1,8 +1,26 @@
 package com.daddeo.test;
 
 public class Test {
+	private String text;
+
 	public void Test() {
 		super();
+		setText( "default" );
+	}
+
+	public void Test(String msg) {
+		super();
+		setText( msg );
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText( String msg ) {
+		if ( text == null )
+			text = new String();
+		text = msg;
 	}
 }
 
